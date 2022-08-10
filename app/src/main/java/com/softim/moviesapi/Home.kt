618e0b19207005665ad9932b4a3e7fdf56b6d5cb
Timo Ruiz <ruiz.timo.ruiz@gmail.com>
2022-08-10 14:03:18 -0500

    Antes de agregar librerias de Hilt y Dagger

--- app/src/main/java/com/softim/moviesapi/Home.kt
+++ app/src/main/java/com/softim/moviesapi/Home.kt
@@ -1,60 +1,29 @@
 package com.softim.moviesapi
 
-import android.Manifest
-import android.R.attr.delay
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.view.Menu
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.softim.moviesapi.databinding.ActivityHomeBinding
-import com.softim.moviesapi.models.ModelUserLocation
-import com.softim.moviesapi.utilities.ExceptionDialogFragment
-import java.util.*
 
 
 class Home : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private val fm: FragmentManager = supportFragmentManager
-    private var bd = Firebase.firestore
-    private lateinit var ref: DocumentReference
-    private val TIEMPO = 300000L
-    private var handler = Handler()
-    private var runnable: Runnable? = null
-    private val CHANNEL_ID = "MOVIESAPI"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        createNotificationChannel()
-        ubicacion()
         setSupportActionBar(binding.appBarHome.toolbar)
 
         binding.appBarHome.fab.setOnClickListener { view ->
@@ -75,120 +44,6 @@ class Home : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-
-    private fun ubicacion() {
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                when {
-                    permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-                        if (ActivityCompat.checkSelfPermission(
-                                this,
-                                Manifest.permission.ACCESS_FINE_LOCATION
-                            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                                this,
-                                Manifest.permission.ACCESS_COARSE_LOCATION
-                            ) == PackageManager.PERMISSION_GRANTED
-                        ) {
-                            fusedLocationClient.lastLocation
-                                .addOnSuccessListener { location: Location? ->
-                                    mensaje(location)
-                                }
-                        }
-                    }
-                    permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                        fusedLocationClient.lastLocation
-                            .addOnSuccessListener { location: Location? ->
-                                mensaje(location)
-                            }
-                    }
-                    else -> {
-                        notificar()
-                    }
-                }
-            }
-        }
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.READ_EXTERNAL_STORAGE))
-    }
-
-    override fun onResume() {
-        ejecutarTarea()
-        super.onResume()
-    }
-
-    override fun onDestroy() {
-        handler.removeCallbacks(runnable!!)
-        super.onDestroy()
-    }
-
-    private fun ejecutarTarea() {
-        handler.postDelayed(Runnable {
-            ubicacion()
-            handler.postDelayed(runnable!!, TIEMPO)
-        }.also { runnable = it }, TIEMPO)
-    }
-
-    private fun notificar() {
-        val message = "You must be granted the permission of location."
-        ExceptionDialogFragment(message).show(fm, ExceptionDialogFragment.TAG)
-        ubicacion()
-    }
-
-    private fun mensaje(location: Location?) {
-        val sharedPreferences = getSharedPreferences("user_movies", MODE_PRIVATE)
-        val user_local = sharedPreferences.getString("user", "")
-        val uniqueID = UUID.randomUUID().toString()
-        if (user_local == "") {
-            val editor = sharedPreferences.edit()
-            editor.putString("user", uniqueID)
-            editor.apply()
-            ref = bd.collection("moviesAPIuser").document(uniqueID)
-                .collection("locations").document()
-        } else {
-            ref = bd.collection("moviesAPIuser").document(user_local!!)
-                .collection("locations").document()
-        }
-
-        val user = ModelUserLocation(uniqueID, location!!.latitude, location.longitude)
-        ref.set(user)
-            .addOnSuccessListener {
-                val message = "Right now you are. \nLatitude: ${location.latitude} \nLongitude: ${location.longitude}"
-                notificationLocation(message)
-            }.addOnFailureListener {
-                val message = "Upload Location Failed"
-                notificationLocation(message)
-            }
-    }
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    private fun notificationLocation(msj: String){
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("Your Location")
-            .setContentText(msj)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-        with(NotificationManagerCompat.from(this)) {
-            notify(2, builder.build())
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home, menu)
--- app/src/main/java/com/softim/moviesapi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/softim/moviesapi/ui/home/HomeFragment.kt
@@ -1,7 +1,13 @@
 package com.softim.moviesapi.ui.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -10,18 +16,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.softim.moviesapi.R
 import com.softim.moviesapi.databinding.FragmentHomeBinding
+import com.softim.moviesapi.models.ModelUserLocation
 import com.softim.moviesapi.models.Model_movie
 import com.softim.moviesapi.utilities.APIservice
 import com.softim.moviesapi.utilities.ExceptionDialogFragment
 import com.softim.moviesapi.utilities.MoviesAdapter
 import com.softim.moviesapi.utilities.MoviesLocalBD
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
@@ -31,12 +48,16 @@ class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener  {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private lateinit var adapter: MoviesAdapter
     private val moviesImages = mutableListOf<Model_movie>()
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var bd = Firebase.firestore
+    private lateinit var ref: DocumentReference
+    private val TIEMPO = 300000L
+    private val CHANNEL_ID = "MOVIESAPI"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,13 +66,16 @@ class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener  {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        createNotificationChannel()
+        ubicacion()
+        ejecutarTarea()
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+
         binding.spnMovies.onItemSelectedListener = this
     }
 
@@ -154,5 +178,121 @@ class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener  {
     override fun onNothingSelected(p0: AdapterView<*>?) {
         val message = "Select Breed of Dog."
         ExceptionDialogFragment(message).show(parentFragmentManager, ExceptionDialogFragment.TAG)
+    }
+
+
+    private fun ubicacion() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                when {
+                    permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+                        if (ActivityCompat.checkSelfPermission(
+                                requireContext(),
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                requireContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED
+                        ) {
+                            fusedLocationClient.lastLocation
+                                .addOnSuccessListener { location: Location? ->
+                                    mensaje(location)
+                                }
+                        }
+                    }
+                    permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        fusedLocationClient.lastLocation
+                            .addOnSuccessListener { location: Location? ->
+                                mensaje(location)
+                            }
+                    }
+                    else -> {
+                        notificar()
+                    }
+                }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun reubicate(){
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                mensaje(location)
+            }
+    }
+
+
+    private fun ejecutarTarea() {
+        GlobalScope.launch(Dispatchers.Main) {
+            while(true) {
+                delay(TIEMPO)
+                reubicate()
+            }
+        }
+    }
+
+    private fun notificar() {
+        val message = "You must be granted the permission of location."
+        ExceptionDialogFragment(message).show(parentFragmentManager, ExceptionDialogFragment.TAG)
+        ubicacion()
+    }
+
+    private fun mensaje(location: Location?) {
+        val sharedPreferences = activity?.getSharedPreferences("user_movies", AppCompatActivity.MODE_PRIVATE)
+        val user_local = sharedPreferences?.getString("user", "")
+        val uniqueID = UUID.randomUUID().toString()
+        if (user_local == "") {
+            val editor = sharedPreferences.edit()
+            editor.putString("user", uniqueID)
+            editor.apply()
+            ref = bd.collection("moviesAPIuser").document(uniqueID)
+                .collection("locations").document()
+        } else {
+            ref = bd.collection("moviesAPIuser").document(user_local!!)
+                .collection("locations").document()
+        }
+
+        val user = ModelUserLocation(uniqueID, location!!.latitude, location.longitude)
+        ref.set(user)
+            .addOnSuccessListener {
+                val message = "Right now you are in \nLatitude: ${location.latitude} \nLongitude: ${location.longitude}"
+                notificationLocation(message)
+            }.addOnFailureListener {
+                val message = "Upload Location Failed"
+                notificationLocation(message)
+            }
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun notificationLocation(msj: String){
+        val builder = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Your Location")
+            .setContentText(msj)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        with(NotificationManagerCompat.from(requireContext())) {
+            notify(2, builder.build())
+        }
     }
 }
